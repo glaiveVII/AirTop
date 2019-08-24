c2bba0aba383285fbc5b983d243f1392f9859e0c
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-24 18:10:16 +0100

    added all policies for this MVC

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
+  include Pundit
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -11,5 +11,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
--- app/models/airdrop.rb
+++ app/models/airdrop.rb
@@ -1,2 +1,8 @@
 class Airdrop < ApplicationRecord
+  belongs_to :airdrop, class_name: "User"
+
+  validates :amount, presence: true
+  validates :crypto, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end

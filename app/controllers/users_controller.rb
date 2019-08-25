a4a91303fd1a841d8d2b7ef8307590b143e77a63
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-25 12:40:58 +0100

    change navbar and basic MVC working need to add a table booking to register and create event

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def show
+    current_user
+    authorize current_user
+  end
+
+  def edit
+    @user = current_user
+    authorize @user
+  end
+
+  def update
+    current_user.update(user_params)
+    @user = current_user
+    authorize @user
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :nickname, :public_key)
+  end
 end

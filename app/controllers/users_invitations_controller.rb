8d25599d60f019d50608e7c24e17a8f0be43ae42
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-27 15:26:34 +0100

    change front and fix bug

--- app/controllers/users_invitations_controller.rb
+++ app/controllers/users_invitations_controller.rb
@@ -1,20 +1,26 @@
 # class UsersInvitationsController < ApplicationController
-# class Users::InvitationsController < Devise::InvitationsController
-#   private
+class Users::InvitationsController < Devise::InvitationsController
 
-#   # this is called when creating invitation
-#   # should return an instance of resource class
-#   def invite_resource
-#     # skip sending emails on invite
-#     super { |user| user.skip_invitation = true }
-#   end
+  def new
 
-#   # this is called when accepting invitation
-#   # should return an instance of resource class
-#   def accept_resource
-#     resource = resource_class.accept_invitation!(update_resource_params)
-#     # Report accepting invitation to analytics
-#     Analytics.report('invite.accept', resource.id)
-#     resource
-#   end
-# end
+  end
+
+
+  # private
+
+  # # this is called when creating invitation
+  # # should return an instance of resource class
+  # def invite_resource
+  #   # skip sending emails on invite
+  #   super { |user| user.skip_invitation = true }
+  # end
+
+  # # this is called when accepting invitation
+  # # should return an instance of resource class
+  # def accept_resource
+  #   resource = resource_class.accept_invitation!(update_resource_params)
+  #   # Report accepting invitation to analytics
+  #   Analytics.report('invite.accept', resource.id)
+  #   resource
+  # end
+end

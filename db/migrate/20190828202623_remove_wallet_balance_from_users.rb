5ec831bf3c5720a26c2ea24288b6b9337becd174
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-29 12:38:41 +0100

    just fix wallet balance string to float, did migration, now lunchhhhhh

--- db/migrate/20190828202623_remove_wallet_balance_from_users.rb
+++ db/migrate/20190828202623_remove_wallet_balance_from_users.rb
@@ -3,3 +3,10 @@ class RemoveWalletBalanceFromUsers < ActiveRecord::Migration[5.2]
     remove_column :users, :wallet_balance, :string
   end
 end
+
+
+# class AddWalletBalanceToUsers < ActiveRecord::Migration[5.2]
+#   def change
+#     add_column :users, :wallet_balance, :string, :default => 0
+#   end
+# end

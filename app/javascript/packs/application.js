2e6a776b31cc1bace8842deca43429113f7dc091
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-31 13:10:16 +0100

    fix front end bugs and improve from feedback

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 
 document.querySelector('.fox-head').addEventListener('click', () => {
-  window.location.href = "https://metamask.iog"
+  window.location.href = "https://metamask.io"
 })
 
 

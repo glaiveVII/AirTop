fdb91a643cd30f8cfc37924884b2498f93b10060
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-09-01 00:01:55 +0100

    imrpove front and ux and add js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,10 @@ document.querySelector('.fox-head').addEventListener('click', () => {
 })
 
 
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('.sweet-alert-demo', {
+  title: "YOUR PURR-FECT FRIEND HAS BEEN CREATED",
+  text: "This is a great KITTY, isn't it?",
+  icon: "success"
+});

42af6ecb86d2d2d60787e17e99587d997b6440e4
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-09-03 11:47:55 +0100

    change a bit the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,12 @@ require 'faker'
 
 puts 'Seed done for airdrop, user'
 
-User.create!(email: 'airtop@gmail.com', password: 'airtop',
+User.create!(email: 'julien@gmail.com', password: 'airtop',
     public_key: "sadlfgjd;fzgadjgjkls;fgjk",
-    nickname: "Julien",
+    nickname: "Pel",
     wallet_balance: 4,
+    first_name: "Julien",
+    last_name: "Pelegri",
     photo: "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/67444491_2393169547624893_3356167537644535808_o.jpg?_nc_cat=104&_nc_oc=AQkprhNozYg7lNipYdrhW4D_tV_M9PsQWYKDL1jvVgvzCNNxOIzr_JJ0Zr1p3P_o6pk&_nc_ht=scontent-lhr3-1.xx&oh=5b84eb83e55523fdc327cf40a54eeb10&oe=5DD5F29A"
     )
 
@@ -40,11 +42,12 @@ User.create!(email: 'lucien@lewagon.com', password: 'airtop',
 
 User.create!(email: 'saunier@lewagon.com', password: 'airtop',
     public_key: "sadlfgjd;fzgpdjgjkls;fgjk",
-    nickname: "Sebastien",
+    nickname: "CodingMaster",
+    first_name: "Sebastien",
+    last_name: "Saunier",
     photo: "https://avatars1.githubusercontent.com/u/414418?s=460&v=4"
     )
 
-quote = ["in btc we trust", "in tech we trust", 'btc to the moon', 'i hodl ripple', 'btc-eth-ltc = <3']
 
 1.times do |i|
   user = User.new(
@@ -66,7 +69,7 @@ quote = ["in btc we trust", "in tech we trust", 'btc to the moon', 'i hodl rippl
     amount: rand(1..15),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
-    quote: quote[i],
+    quote: "Vitalik where is new ETH?",
     description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
     # photo: "https://ludu-assets.s3.amazonaws.com/course-icons/26/xEtx9RpWSk24kkXaRicP"
     remote_photo_url: "https://ih0.redbubble.net/image.536846432.7858/flat,550x550,075,f.u1.jpg"
@@ -95,7 +98,7 @@ end
     amount: rand(1..30),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
-    quote: quote[i],
+    quote: "I hodl ripple, lol",
     description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
     # photo: "https://banner2.kisspng.com/20180805/hql/kisspng-litecoin-bitcoin-cryptocurrency-wallet-digital-wal-bitconnect-wallet-mac-bitcoin-cash-transaction-s-5b667f911cfec0.1647560515334439851188.jpg"
     remote_photo_url: "http://cdn.onlinewebfonts.com/svg/img_103118.png"
@@ -124,7 +127,7 @@ end
     amount: rand(1..5),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
-    quote: quote[i],
+    quote: "in btc we trust",
     description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
     # photo: "https://png.icons8.com/dotty/1600/673AB7/bitcoin",
     remote_photo_url: "https://upload.wikimedia.org/wikipedia/commons/9/9a/BTC_Logo.svg"
@@ -153,7 +156,7 @@ end
     amount: rand(1..30),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
-    quote: quote[i],
+    quote: "LTC to the moon",
     description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
     # photo: "https://banner2.kisspng.com/20180805/hql/kisspng-litecoin-bitcoin-cryptocurrency-wallet-digital-wal-bitconnect-wallet-mac-bitcoin-cash-transaction-s-5b667f911cfec0.1647560515334439851188.jpg"
     remote_photo_url: "http://cdn.onlinewebfonts.com/svg/img_103118.png"
@@ -182,7 +185,7 @@ end
     amount: rand(1..5),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
-    quote: quote[i],
+    quote: "In POW we don't trust",
     description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
     # photo: "https://png.icons8.com/dotty/1600/673AB7/bitcoin",
     remote_photo_url: "https://upload.wikimedia.org/wikipedia/commons/9/9a/BTC_Logo.svg"
@@ -211,7 +214,7 @@ end
     amount: rand(1..15),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
-    quote: quote[i],
+    quote: "ETH <3",
     description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
     # photo: "https://ludu-assets.s3.amazonaws.com/course-icons/26/xEtx9RpWSk24kkXaRicP"
     remote_photo_url: "https://ih0.redbubble.net/image.536846432.7858/flat,550x550,075,f.u1.jpg"

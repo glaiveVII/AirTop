c253213bc967297a9b1f76350a9aca4b26534976
Ali Kalkanel <kally-95@hotmail.com>
2019-09-03 11:55:17 +0100

    fixed bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,34 +12,34 @@ require 'faker'
 puts 'Seed done for airdrop, user'
 
 User.create!(email: 'airtop@gmail.com', password: 'airtop',
-    public_key: "sadlfgjd;fzgadjgjkls;fgjk",
+    public_key: "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh",
     nickname: "Julien",
     wallet_balance: 4,
     photo: "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/67444491_2393169547624893_3356167537644535808_o.jpg?_nc_cat=104&_nc_oc=AQkprhNozYg7lNipYdrhW4D_tV_M9PsQWYKDL1jvVgvzCNNxOIzr_JJ0Zr1p3P_o6pk&_nc_ht=scontent-lhr3-1.xx&oh=5b84eb83e55523fdc327cf40a54eeb10&oe=5DD5F29A"
     )
 
 User.create!(email: 'airtop@hotmail.com', password: 'airtop',
-    public_key: "sadlfgjd;fzgpdjgjkls;fgjk",
+    public_key: "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh",
     nickname: "Paul")
 
 User.create!(email: 'marco@lewagon.com', password: 'airtop',
-    public_key: "sadlfgjd;fzgpdjgjkls;fgjk",
+    public_key: "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh",
     nickname: "Marco")
 
 User.create!(email: 'sam@lewagon.com', password: 'airtop',
-    public_key: "sadlfgjd;fzgpdjgjkls;fgjk",
+    public_key: "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh",
     nickname: "Sam")
 
 User.create!(email: 'arthur@lewagon.com', password: 'airtop',
-    public_key: "sadlfgjd;fzgpdjgjkls;fgjk",
+    public_key: "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh",
     nickname: "Arthur")
 
 User.create!(email: 'lucien@lewagon.com', password: 'airtop',
-    public_key: "sadlfgjd;fzgpdjgjkls;fgjk",
+    public_key: "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh",
     nickname: "Lucien")
 
 User.create!(email: 'saunier@lewagon.com', password: 'airtop',
-    public_key: "sadlfgjd;fzgpdjgjkls;fgjk",
+    public_key: "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh",
     nickname: "Sebastien",
     photo: "https://avatars1.githubusercontent.com/u/414418?s=460&v=4"
     )
@@ -58,16 +58,16 @@ quote = ["in btc we trust", "in tech we trust", 'btc to the moon', 'i hodl rippl
   user.password = 123456789
   user.admin = false
   user.wallet_balance = 0
-  user.public_key = "sadlfgjd;fzgadjgjkls;fgjk"
+  user.public_key = "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh"
   user.save!
 
   airdrop = Airdrop.new(
-    crypto: "ethereum",
+    crypto: "Ethereum",
     amount: rand(1..15),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     quote: quote[i],
-    description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
+    description: "Ninja giveaway, a big thank you to my supportive Community. Share love, share Crypto.",
     # photo: "https://ludu-assets.s3.amazonaws.com/course-icons/26/xEtx9RpWSk24kkXaRicP"
     remote_photo_url: "https://ih0.redbubble.net/image.536846432.7858/flat,550x550,075,f.u1.jpg"
   )
@@ -87,16 +87,16 @@ end
   user.password = 123456789
   user.wallet_balance = 0
   user.admin = false
-  user.public_key = "sadlfgjd;fzgadjgjkls;fgjk"
+  user.public_key = "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh"
   user.save!
 
   airdrop = Airdrop.new(
-    crypto: "litecoin",
+    crypto: "Litecoin",
     amount: rand(1..30),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     quote: quote[i],
-    description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
+    description: "Giving away something Lite, not too heavy (like Gold). That you can recieve in the matter of minutes.",
     # photo: "https://banner2.kisspng.com/20180805/hql/kisspng-litecoin-bitcoin-cryptocurrency-wallet-digital-wal-bitconnect-wallet-mac-bitcoin-cash-transaction-s-5b667f911cfec0.1647560515334439851188.jpg"
     remote_photo_url: "http://cdn.onlinewebfonts.com/svg/img_103118.png"
   )
@@ -116,16 +116,17 @@ end
   user.password = 123456789
   user.admin = false
   user.wallet_balance = 0
-  user.public_key = "sadlfgjd;fzgadjgjkls;fgjk"
+  user.public_key = "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh"
   user.save!
 
   airdrop = Airdrop.new(
-    crypto: "bitcoin",
+    crypto: "Bitcoin",
     amount: rand(1..5),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     quote: quote[i],
-    description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
+    description: "If you own 1 BTC, you are mathematically guaranteed to be top 3/1000 richest in the world, in BTC terms (21/
+    m/7B)",
     # photo: "https://png.icons8.com/dotty/1600/673AB7/bitcoin",
     remote_photo_url: "https://upload.wikimedia.org/wikipedia/commons/9/9a/BTC_Logo.svg"
   )
@@ -145,16 +146,16 @@ end
   user.password = 123456789
   user.wallet_balance = 0
   user.admin = false
-  user.public_key = "sadlfgjd;fzgadjgjkls;fgjk"
+  user.public_key = "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh"
   user.save!
 
   airdrop = Airdrop.new(
-    crypto: "litecoin",
+    crypto: "Litecoin",
     amount: rand(1..30),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     quote: quote[i],
-    description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
+    description: "Litecoin giveaway, a fork of Bitcoin.",
     # photo: "https://banner2.kisspng.com/20180805/hql/kisspng-litecoin-bitcoin-cryptocurrency-wallet-digital-wal-bitconnect-wallet-mac-bitcoin-cash-transaction-s-5b667f911cfec0.1647560515334439851188.jpg"
     remote_photo_url: "http://cdn.onlinewebfonts.com/svg/img_103118.png"
   )
@@ -174,16 +175,16 @@ end
   user.password = 123456789
   user.admin = false
   user.wallet_balance = 0
-  user.public_key = "sadlfgjd;fzgadjgjkls;fgjk"
+  user.public_key = "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh"
   user.save!
 
   airdrop = Airdrop.new(
-    crypto: "bitcoin",
+    crypto: "Bitcoin",
     amount: rand(1..5),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     quote: quote[i],
-    description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
+    description: "Bitcoin gaming pool, giving away aidr to E-sports players.",
     # photo: "https://png.icons8.com/dotty/1600/673AB7/bitcoin",
     remote_photo_url: "https://upload.wikimedia.org/wikipedia/commons/9/9a/BTC_Logo.svg"
   )
@@ -203,16 +204,16 @@ end
   user.password = 123456789
   user.admin = false
   user.wallet_balance = 0
-  user.public_key = "sadlfgjd;fzgadjgjkls;fgjk"
+  user.public_key = "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh"
   user.save!
 
   airdrop = Airdrop.new(
-    crypto: "ethereum",
+    crypto: "Ethereum",
     amount: rand(1..15),
     user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     quote: quote[i],
-    description: "Random description about the crypto event, when lambo moon, xrp will replace fiat currency tomorrow",
+    description: "A public blockchain based distributed computing platform featuring smart contract functionality.",
     # photo: "https://ludu-assets.s3.amazonaws.com/course-icons/26/xEtx9RpWSk24kkXaRicP"
     remote_photo_url: "https://ih0.redbubble.net/image.536846432.7858/flat,550x550,075,f.u1.jpg"
   )

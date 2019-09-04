c257a330f6a9c42622c32d54531180e7b575b640
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-09-04 10:55:11 +0100

    debrief of the meeting, fixed some stuff but lot to do still

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ User.create!(email: 'julien@gmail.com', password: 'airtop',
     public_key: public_key_btc_1,
     public_key_eth: public_key_eth_1,
     public_key_ltc: public_key_ltc_1,
-    nickname: "Pel",
+    nickname: "Julien",
     wallet_balance: wallet_balance_btc_1,
     wallet_balance_eth: wallet_balance_eth_1,
     wallet_balance_ltc: wallet_balance_ltc_1,
@@ -63,7 +63,7 @@ User.create!(email: 'lucien@lewagon.com', password: 'airtop',
 
 User.create!(email: 'saunier@lewagon.com', password: 'airtop',
     public_key: "93WZu1LBjzjiwQScYpr5JNeIlTKLQ3BbpKIy28zDiwWXXb7drmNh",
-    nickname: "CodingMaster",
+    nickname: "SEB",
     public_key: public_key_btc_2,
     public_key_eth: public_key_eth_2,
     public_key_ltc: public_key_ltc_2,
@@ -94,13 +94,13 @@ User.create!(email: 'saunier@lewagon.com', password: 'airtop',
   airdrop = Airdrop.new(
     crypto: "Ethereum",
     amount: rand(1..15),
-    user_id: User.find_by_nickname("Pel").id,
+    user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     description: "Ninja giveaway, a big thank you to my supportive Community. Share love, share Crypto.",
     quote: "Vitalik where is new ETH?",
 
     # photo: "https://ludu-assets.s3.amazonaws.com/course-icons/26/xEtx9RpWSk24kkXaRicP"
-    remote_photo_url: "https://ih0.redbubble.net/image.536846432.7858/flat,550x550,075,f.u1.jpg"
+    remote_photo_url: "https://media.licdn.com/dms/image/C4D0BAQHszSGuo_jTgA/company-logo_200_200/0?e=2159024400&v=beta&t=PsyPmw2ZADCmXsYJKfqgkCxsUbo7l-e_4jS_HkjLj_g"
   )
 
   airdrop.save!
@@ -124,12 +124,12 @@ end
   airdrop = Airdrop.new(
     crypto: "Litecoin",
     amount: rand(1..30),
-    user_id: User.find_by_nickname("Pel").id,
+    user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     description: "Giving away something Lite, not too heavy (like Gold). That you can recieve in the matter of minutes.",
-    quote: "I hodl ripple, lol",
+    quote: "I HODL XRP, lol",
     # photo: "https://banner2.kisspng.com/20180805/hql/kisspng-litecoin-bitcoin-cryptocurrency-wallet-digital-wal-bitconnect-wallet-mac-bitcoin-cash-transaction-s-5b667f911cfec0.1647560515334439851188.jpg"
-    remote_photo_url: "http://cdn.onlinewebfonts.com/svg/img_103118.png"
+    remote_photo_url: "http://www.legrowspot.com/wp-content/uploads/2017/08/LGS__0003_logo_societe-generale.png"
   )
 
   airdrop.save!
@@ -153,13 +153,13 @@ end
   airdrop = Airdrop.new(
     crypto: "Bitcoin",
     amount: rand(1..5),
-    user_id: User.find_by_nickname("Pel").id,
+    user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     description: "If you own 1 BTC, you are mathematically guaranteed to be top 3/1000 richest in the world, in BTC terms (21/
     m/7B)",
-    quote: "in btc we trust",
+    quote: "in BTC we TRUST",
     # photo: "https://png.icons8.com/dotty/1600/673AB7/bitcoin",
-    remote_photo_url: "https://upload.wikimedia.org/wikipedia/commons/9/9a/BTC_Logo.svg"
+    remote_photo_url: "https://thespun.com/wp-content/uploads/2017/08/Screen-Shot-2017-08-16-at-4.37.35-PM-775x465.png"
   )
 
   airdrop.save!
@@ -183,12 +183,12 @@ end
   airdrop = Airdrop.new(
     crypto: "Litecoin",
     amount: rand(1..30),
-    user_id: User.find_by_nickname("Pel").id,
+    user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     description: "Litecoin giveaway, a fork of Bitcoin.",
-    quote: "LTC to the moon",
+    quote: "LTC to the MOON",
     # photo: "https://banner2.kisspng.com/20180805/hql/kisspng-litecoin-bitcoin-cryptocurrency-wallet-digital-wal-bitconnect-wallet-mac-bitcoin-cash-transaction-s-5b667f911cfec0.1647560515334439851188.jpg"
-    remote_photo_url: "http://cdn.onlinewebfonts.com/svg/img_103118.png"
+    remote_photo_url: "https://yt3.ggpht.com/a/AGF-l7-BBIcC888A2qYc3rB44rST01IEYDG3uzbU_A=s900-c-k-c0xffffffff-no-rj-mo"
   )
 
   airdrop.save!
@@ -212,13 +212,13 @@ end
   airdrop = Airdrop.new(
     crypto: "Bitcoin",
     amount: rand(1..5),
-    user_id: User.find_by_nickname("Pel").id,
+    user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
 
     description: "Bitcoin gaming pool, giving away aidr to E-sports players.",
-    quote: "In POW we don't trust",
+    quote: "In POW we DOn't trust",
     # photo: "https://png.icons8.com/dotty/1600/673AB7/bitcoin",
-    remote_photo_url: "https://upload.wikimedia.org/wikipedia/commons/9/9a/BTC_Logo.svg"
+    remote_photo_url: "https://pbs.twimg.com/profile_images/1139570739747536896/HVEeWmV4.png"
   )
 
   airdrop.save!
@@ -242,24 +242,24 @@ end
   airdrop = Airdrop.new(
     crypto: "Ethereum",
     amount: rand(1..15),
-    user_id: User.find_by_nickname("Pel").id,
+    user_id: User.find_by_nickname("Julien").id,
     # address: Faker::Address.street_address,
     description: "A public blockchain based distributed computing platform featuring smart contract functionality.",
     quote: "ETH <3",
     # photo: "https://ludu-assets.s3.amazonaws.com/course-icons/26/xEtx9RpWSk24kkXaRicP"
-    remote_photo_url: "https://ih0.redbubble.net/image.536846432.7858/flat,550x550,075,f.u1.jpg"
+    remote_photo_url: "https://image.freepik.com/icones-gratuites/mac-os_318-10374.jpg"
   )
 
   airdrop.save!
 end
 
 Invite.create!(airdrop_id: Airdrop.first.id,
-               user_id: User.find_by_nickname("Pel").id,
+               user_id: User.find_by_nickname("Julien").id,
                status: "accepted"
                )
 
 Invite.create!(airdrop_id: Airdrop.last.id,
-               user_id: User.find_by_nickname("Pel").id,
+               user_id: User.find_by_nickname("Julien").id,
                status: "accepted"
                )
 
